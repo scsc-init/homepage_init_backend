4aea7513163646ddab5f2020fa7e0ab88607cb0f
MS Kang <tomskang@naver.com>
2025-05-10 00:00:58 +0900

    style: Line1에 파일명 명시, import 상단에 주석 작성, middleware 순서를 변경, https_only 설정을 False로 변경

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+"""/main.py"""
+
+# Dependency
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 
+# Middleware
 from src.middleware.api_secret import APISecretMiddleware
-from src.core.config import get_session_secret
 from src.middleware.assert_permission import AssertPermissionMiddleware
+# Environment
+from src.core.config import get_session_secret
+# Route
 from src.routes.root import root_router
 
 app = FastAPI()
 
 app.add_middleware(AssertPermissionMiddleware)
+app.add_middleware(APISecretMiddleware)
 app.add_middleware(
     SessionMiddleware,
     secret_key=get_session_secret(),
     session_cookie="session_id",
-    max_age=1800, # seconds
+    max_age=1800, # 1800 sec = 30 min
     # same_site="strict",
-    https_only=True,
+    https_only=False,
 )
-app.add_middleware(APISecretMiddleware)
 
 app.include_router(root_router)
