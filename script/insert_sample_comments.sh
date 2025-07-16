7d66161c597a90f4f56febba02a8363084adbb25
NieunA <stareuna06@gmail.com>
2025-07-16 14:30:00 +0900

    chore: add script/insert_sample_comments.sh

---
+++ script/insert_sample_comments.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if DB file argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <database_file>"
+  exit 1
+fi
+
+DB_FILE="$1"
+
+# Check if the database file exists
+if [ ! -f "$DB_FILE" ]; then
+  echo "Error: Database file '$DB_FILE' does not exist."
+  exit 1
+fi
+
+# Insert Sample Comment on Sample Article with President ID(president@example.com)
+sqlite3 "$DB_FILE" <<EOF
+INSERT INTO comment (id, content, author_id, article_id, parent_id) VALUES (0, 'Sample Comment', 'fc2eef7ad1e1f2f91786b4fd4f65508651373b0ad6fc4f13103c452369bc9703', 0, null);
+EOF
+
+echo "Sample comment(s) inserted successfully into '$DB_FILE'."
