175907e10d7140b483861d4de1cd0275c1d887d9
이태윤 <dlxodbs0908@gmail.com>
2025-10-07 04:22:09 +0900

    7

---
+++ script/migrations/5.sql
@@ -0,0 +1,21 @@
+﻿PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS key_value (
+    key TEXT PRIMARY KEY,
+    value TEXT NOT NULL,
+    writing_permission_level INTEGER NOT NULL
+        REFERENCES user_role(level) ON DELETE RESTRICT,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT OR IGNORE INTO key_value (key, value, writing_permission_level)
+VALUES ('footer-message', '서울대학교 컴퓨터 연구회\n회장 한성재 010-5583-1811\nscsc.snu@gmail.com', 500);
+
+CREATE TRIGGER IF NOT EXISTS trg_key_value_updated_at
+AFTER UPDATE ON key_value
+FOR EACH ROW
+BEGIN
+    UPDATE key_value
+    SET updated_at = CURRENT_TIMESTAMP
+    WHERE key = OLD.key;
+END;
