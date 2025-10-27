1f502442ed1b954bab5ff65d236f74bd93cfc63d
1n1tial <dan.yun0821@gmail.com>
2025-10-28 00:23:37 +0900

    ⚡️ refactor db init and migration

--- script/init_db/insert_kv.sh
+++ script/init_db/insert_kv.sh
@@ -18,7 +18,8 @@ fi
 
 sqlite3 "$DB_FILE" <<EOF
 INSERT INTO key_value (key, value, writing_permission_level) VALUES ('footer-message', '서울대학교 컴퓨터 연구회\n회장 한성재 010-5583-1811\nscsc.snu@gmail.com', 500);
-INSERT INTO key_value (key, value, writing_permission_level) VALUES ('leaders', NULL, 500);
+INSERT INTO key_value (key, value, writing_permission_level) VALUES ('president', NULL, 500);
+INSERT INTO key_value (key, value, writing_permission_level) VALUES ('vice-president', NULL, 500);
 EOF
 
 echo "All key values inserted successfully into '$DB_FILE'."
