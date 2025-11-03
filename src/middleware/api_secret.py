e86e5e0b218da176e090636cee121244b73a4d07
이태윤 <dlxodbs0908@gmail.com>
2025-11-03 16:15:43 +0900

    3

--- src/middleware/api_secret.py
+++ src/middleware/api_secret.py
@@ -1,4 +1,4 @@
-from fastapi import Request, HTTPException
+from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 
