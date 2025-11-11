baf004c888d7b8c3a4f15c26428ebfa7c52eba9d
1n1tial <dan.yun0821@gmail.com>
2025-11-11 11:22:14 +0900

    fix typo

--- src/routes/scsc.py
+++ src/routes/scsc.py
@@ -7,21 +7,21 @@ from src.util import get_user
 scsc_router = APIRouter(tags=["scsc"])
 
 
-@scsc_router.get("/global/status")
+@scsc_router.get("/scsc/global/status")
 async def get_scsc_global_status(
     scsc_service: SCSCServiceDep,
 ) -> SCSCGlobalStatus:
     return scsc_service.get_global_status()
 
 
-@scsc_router.get("/global/statuses")
+@scsc_router.get("/scsc/global/statuses")
 async def get_scsc_global_statuses(
     scsc_service: SCSCServiceDep,
 ) -> dict[str, list[str]]:
     return scsc_service.get_all_statuses()
 
 
-@scsc_router.post("/executive/global/status", status_code=204)
+@scsc_router.post("/executive/scsc/global/status", status_code=204)
 async def update_scsc_global_status(
     request: Request,
     body: BodyUpdateSCSCGlobalStatus,
