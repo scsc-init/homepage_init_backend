954bcd1845859a29bd8204cfc2ceefe38f7c6e16
박성현 <54495942+coshaman@users.noreply.github.com>
2025-12-25 21:18:03 +0900

    Update api_secret.py

--- src/dependencies/api_secret.py
+++ src/dependencies/api_secret.py
@@ -4,13 +4,6 @@ from src.core import get_settings
 
 
 async def api_secret(request: Request):
-    path = request.url.path
-    if path not in (
-        "/api/user/login",
-        "/api/user/create",
-    ):
-        return
-
     x_api_secret = request.headers.get("x-api-secret")
     if x_api_secret is None:
         raise HTTPException(status_code=401, detail="No x-api-secret included")
