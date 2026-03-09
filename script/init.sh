5c9554128872ee187331b011db517fc52b17f6e3
HK Lee <69958270+tteokgook1@users.noreply.github.com>
2026-03-10 00:35:35 +0900

    fix review

--- script/init.sh
+++ script/init.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username postgres --dbname "${DB_NAME:-main_db}" <<-EOSQL
-    -- 1. App User with Env Password
-    CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
-    GRANT ALL ON DATABASE ${DB_NAME:-main_db} TO $DB_USER;
-    GRANT ALL ON ALL TABLES IN SCHEMA public TO $DB_USER;
-    GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO $DB_USER;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO $DB_USER;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO $DB_USER;
+psql -v ON_ERROR_STOP=1 --username postgres --dbname "${DB_NAME:-main_db}" \
+    -v db_name="${DB_NAME:-main_db}" \
+    -v db_user="$DB_USER" \
+    -v db_password="$DB_PASSWORD" \
+    -v ro_password="$READONLY_PASSWORD" <<-EOSQL
 
-    -- 2. ReadOnly User with Env Password
-    CREATE USER readonly_user WITH PASSWORD '$READONLY_PASSWORD';
-    GRANT CONNECT ON DATABASE ${DB_NAME:-main_db} TO readonly_user;
+    SELECT format('CREATE USER %I WITH PASSWORD %L', :'db_user', :'db_password') \gexec
+    SELECT format('GRANT ALL PRIVILEGES ON DATABASE %I TO %I', :'db_name', :'db_user') \gexec
+    SELECT format('CREATE USER readonly_user WITH PASSWORD %L', :'ro_password') \gexec
+    SELECT format('GRANT CONNECT ON DATABASE %I TO readonly_user', :'db_name') \gexec
+
+    GRANT USAGE ON SCHEMA public TO :"db_user";
+    GRANT ALL ON ALL TABLES IN SCHEMA public TO :"db_user";
+    GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO :"db_user";
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO :"db_user";
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO :"db_user";
+
     GRANT USAGE ON SCHEMA public TO readonly_user;
     GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
+    GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly_user;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly_user;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO readonly_user;
 EOSQL
