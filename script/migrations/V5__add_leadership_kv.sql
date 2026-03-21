0c6925b5fbe4d48198a97128df199cddd72a8b1a
taeyoon0908 <ty0908@snu.ac.kr>
2026-03-21 11:16:18 +0900

    contact us kv화 수정 1.2차

--- script/migrations/V5__add_leadership_kv.sql
+++ script/migrations/V5__add_leadership_kv.sql
@@ -1,7 +1,7 @@
 INSERT INTO key_value (key, value, writing_permission_level)                    
 VALUES
-  ('president-name', '강명석', 500),
-  ('vice-president-name', '박상혁;박성현', 500),                                                                          
-  ('president-phone', '010-2058-7356',500),                                                      
-  ('vice-president-phone', '010-4014-1871;010-3537-2998',500)                                                  
+  ('president-name', 'XXX', 500),
+  ('vice-president-name', 'XXX;XXX', 500),                                                                          
+  ('president-phone', '010-XXXX-XXXX',500),                                                      
+  ('vice-president-phone', '010-XXXX-XXXX;010-XXXX-XXXX',500)                                                  
 ON CONFLICT (key) DO NOTHING;   
