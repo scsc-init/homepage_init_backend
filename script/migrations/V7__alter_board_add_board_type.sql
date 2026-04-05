f290b3e16460f2db57309f45f634f8cccb5ba508
dolphin2410 <dolphin2410@outlook.kr>
2026-04-06 03:28:15 +0900

    :zap: Smarter data update

--- script/migrations/V7__alter_board_add_board_type.sql
+++ script/migrations/V7__alter_board_add_board_type.sql
@@ -5,10 +5,7 @@ ADD CONSTRAINT match_constraint CHECK (board_type IN ('IMAGE', 'TEXT', 'NONE', '
 UPDATE public.board
 SET
     board_type = CASE id
-        WHEN 1 THEN 'NONE'
-        WHEN 2 THEN 'NONE'
-        WHEN 3 THEN 'TEXT'
-        WHEN 4 THEN 'IMAGE'
-        WHEN 5 THEN 'TEXT'
-        WHEN 6 THEN 'TEXT'
-    END
+        WHEN name = 'Album' THEN 'IMAGE'
+        WHEN name IN ('Sig', 'Pig') THEN 'NONE'
+        ELSE 'TEXT'
+    END;
