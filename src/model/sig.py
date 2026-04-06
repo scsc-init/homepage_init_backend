410b9ea5631733b9bc250bfe8b1c68c898bb5728
이수민 <65718685+mickey0605@users.noreply.github.com>
2026-04-06 11:05:47 +0900

    ⚡Change is_rolling_admission type in Sig table (#292)
    * ⚡Change is_rolling_admission type in Sig table
    
    * 리뷰 반영 수정
    
    * 리뷰 반영 수정2
    
    * lint야 나 좀 보내줘
    
    * lint야 찐막이야
    
    * 추가 이슈&리뷰 반영
    
    * 다양한 수정
    
    * 권한 755로 바꾸기
    
    * 코드레빗 수정
    
    * 찐막
    
    * 아 저장을 안하고 올렸네

--- src/model/sig.py
+++ src/model/sig.py
@@ -17,6 +17,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.util import utcnow
 
 from .base import Base
+from .pig import RollingAdmission
 from .scsc_global_status import SCSCStatus
 from .user import UserSummary
 
@@ -60,8 +61,15 @@ class SIG(Base):
     semester: Mapped[int] = mapped_column(Integer, nullable=False)
     owner: Mapped[str] = mapped_column(String, ForeignKey("user.id"), nullable=False)
     should_extend: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    is_rolling_admission: Mapped[bool] = mapped_column(
-        Boolean, default=False, nullable=False
+    is_rolling_admission: Mapped[RollingAdmission] = mapped_column(
+        Enum(
+            RollingAdmission,
+            name="rolling_admission_enum",
+            native_enum=False,
+            values_callable=lambda obj: [e.value for e in obj],
+        ),
+        nullable=False,
+        default=RollingAdmission.DURING_RECRUITING,
     )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=False),
