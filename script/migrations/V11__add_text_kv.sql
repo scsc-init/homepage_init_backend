babf927b91974e79efd0c85cb14bffd63b7a40a8
mickey0605 <mickeylee1698@gmail.com>
2026-05-16 00:15:53 +0900

    1

---
+++ script/migrations/V11__add_text_kv.sql
@@ -0,0 +1,7 @@
+INSERT INTO key_value (key, value, writing_permission_level)
+VALUES
+  ('TEXT_DEPOSIT_ACC', '국민은행 942902-02-054136 (강명석)', 500),
+  ('TEXT_DISCORD_INVITE_LINK', 'https://discord.gg/SmXFDxA7XE', 500),
+  ('TEXT_KAKAO_INVITE_LINK', 'https://invite.kakao.com/tc/II2yiLsQhY', 500)
+ON CONFLICT (key) DO NOTHING;
+ 
