18a0a0c46c2763cd4173815a3584cfc596016b7f
taeyoon0908 <ty0908@snu.ac.kr>
2026-05-24 19:51:44 +0900

    w 계열 페이지 다중 / 지원

--- src/routes/w.py
+++ src/routes/w.py
@@ -1,6 +1,6 @@
-from typing import Sequence
+from typing import Annotated, Sequence
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, Form, UploadFile
 from fastapi.responses import FileResponse
 
 from src.dependencies import UserDep
@@ -16,12 +16,13 @@ async def upload_file(
     current_user: UserDep,
     file: UploadFile,
     w_service: WServiceDep,
+    name: Annotated[str | None, Form()] = None,
 ) -> WHTMLMetadataResponse:
-    w_meta = await w_service.upload_file(current_user, file)
+    w_meta = await w_service.upload_file(current_user, file, name)
     return WHTMLMetadataResponse.model_validate(w_meta)
 
 
-@w_router.get("/w/{name}")
+@w_router.get("/w/{name:path}")
 async def get_w_by_name(name: str, w_service: WServiceDep) -> FileResponse:
     return w_service.get_w_by_name(name)
 
@@ -33,7 +34,7 @@ async def get_all_metadata(
     return w_service.get_all_metadata()
 
 
-@w_router.post("/executive/w/{name}/update", status_code=200)
+@w_router.post("/executive/w/{name:path}/update", status_code=200)
 async def update_w_by_name(
     name: str,
     current_user: UserDep,
@@ -44,7 +45,7 @@ async def update_w_by_name(
     return WHTMLMetadataResponse.model_validate(w_meta)
 
 
-@w_router.post("/executive/w/{name}/delete", status_code=204)
+@w_router.post("/executive/w/{name:path}/delete", status_code=204)
 async def delete_w_by_name(
     name: str,
     current_user: UserDep,
@@ -53,7 +54,7 @@ async def delete_w_by_name(
     await w_service.delete_w_by_name(name, current_user)
 
 
-@w_router.get("/executive/w/{name}/download")
+@w_router.get("/executive/w/{name:path}/download")
 async def download_w_by_name(
     name: str,
     current_user: UserDep,
